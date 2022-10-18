38acf3dc281b36d477dd722256f24f4d2e9179a5
Zaya <zayadeveloper8@gmail.com>
2022-10-17 22:51:43 -0500

    add finance storge

--- app.js
+++ app.js
@@ -1,29 +1,92 @@
 //Delgetstei ajillah controller
 var uiController = (function(){
-  
+    var DOMstrings ={
+      inputType: ".add__type",
+      inputDescription: ".add__description",
+      inputValue: ".add__value",
+      addBtn: ".add__btn"
+    };
+
+    return {
+      getInput: function(){
+        return {
+            type: document.querySelector(DOMstrings.inputType).value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+        };
+      },
+
+      getDOMstrings: function(){
+        return DOMstrings;
+      }
+    };
 })();
 //Sankhuutei ajillah controller
 var financeController = (function(){
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  
+  var incomes =[];
+  var expenses =[];
+
+  var totalIncomes = 0;
+  var totalExpenses = 0;
+
+  var data ={
+    allItems: {
+      inc:[],
+      exp:[]
+    },
+
+    total: {
+      inc:0,
+      exp:0
+    }
+  }
 })();
 
 //Programiin holbogch controller
 var appController = (function(uiController, financeController){
     var ctrlAddItem = function(){
-         //Oruulah ugugdliig delgetsees olj avna
-    console.log("Delgetsnees ogogdloo avag heseg");
-    //olj avsan ugugdluudee sankhuugiin controllert damjuulj hadgalah
-    //Olj avsan ugugdluudiig web deeree tohirgoo
+      //1.Oruulah ugugdliig delgetsees olj avna
+      console.log(uiController.getInput());
+      //2.Olj avsan ugugdluudee sankhuugiin controllert damjuulj hadgalah
+      //3.Olj avsan ugugdluudiig web deeree tohirgoo
+      //4.Tusviig tootsoolno
+      //5.Etssiin uldegdel, tootsoog delgetsend gargana
     };
-document.querySelector('.add__btn').addEventListener('click', function(){
-   ctrlAddItem();
-});
 
-document.addEventListener("keypress", function(event) {
-    if(event.keyCode === 13 || event.which === 13){
-     ctrlAddItem();
+  var setupEventListeners = function(){
+    var DOM = uiController.getDOMstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+      ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function(event) {
+      if(event.keyCode === 13 || event.which === 13){
+      ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function() {
+      console.log("Appliction started...");
+      setupEventListeners();
     }
-    else console.log("Oor toch darsan baina : " + event.keyCode);
-});
+  }
+
 
 })( uiController, financeController);
+
+appController.init();
